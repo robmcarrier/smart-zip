ca04cd9a122fa3dd977860209a60322ec25f8e89
Robert Carrier <robmcarrier@gmail.com>
2016-11-03 10:50:29 -0700

    add campaign info page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :welcome
   resources :user_profiles
+  resources :campaigns
 
   root 'welcome#home'
   # The priority is based upon order of creation: first created -> highest priority.
