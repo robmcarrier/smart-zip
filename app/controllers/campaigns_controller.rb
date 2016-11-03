a221270443e85e6277f2c6c49ef3e16a76edf17e
Robert Carrier <robmcarrier@gmail.com>
2016-11-03 11:35:00 -0700

    add bootstrap

--- app/controllers/campaigns_controller.rb
+++ app/controllers/campaigns_controller.rb
@@ -1,6 +1,23 @@
 class CampaignsController < ApplicationController
 
+  def new
+    @campaign = Campaign.new
+    @campaign.user_id = current_user.id
+  end
+
+  def create
+    @campaign = Campaign.create(campaign_params)
+    redirect_to campaign_path(@campaign)
+  end
+
   def show
     @campaign = Campaign.find(params[:id])
   end
+
+  private
+
+  def campaign_params
+    params.require(:campaign).permit(:user_id, :name, :type)
+  end
+
 end
